fd8397aaf5765581c094c8cd068b9bb5335eef6d
Scott Zimmerman <bighead.0@gmail.com>
2025-05-05 05:04:16 -0400

    Increment version to 1.4.

--- Initialization.cs
+++ Initialization.cs
@@ -9,7 +9,7 @@ namespace ClairObscurConfig
         static void Main()
         {
             // The version of the configurator. Set here for easy editing.
-            Config.AppVersion = "1.3";
+            Config.AppVersion = "1.4";
 
             // Enable form visual styles.
             Application.EnableVisualStyles();
